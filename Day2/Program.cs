a54bc2b62218bb77942c659e38d935e88c3d0ee5
TheRafikiPL <48444061+TheRafikiPL@users.noreply.github.com>
2022-12-02 10:40:06 +0100

    Day 2

---
+++ Day2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Numerics;
+
+namespace Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //PART 1
+            Console.WriteLine("PART 1");
+            string file = "tournament.txt";
+            StreamReader sr = new StreamReader($@"Data\{file}");
+
+            List<Round> rounds = new List<Round>();
+
+            int sumOfPoints = 0;
+
+            while(!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                rounds.Add(new Round(line[0], line[2]));
+            }
+            sr.Close();
+
+            foreach (Round r in rounds)
+            {
+                sumOfPoints += r.PlayerScore;
+            }
+            Console.WriteLine($"Total Score: {sumOfPoints}");
+
+            //PART 2
+            Console.WriteLine("\nPART 2");
+            sr = new StreamReader($@"Data\{file}");
+
+            rounds = new List<Round>();
+
+            sumOfPoints = 0;
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                ChosenFigure cf= CalculateChosenFigure(line[0], line[2]);
+                
+                rounds.Add(new Round(line[0], cf));
+            }
+            sr.Close();
+
+            foreach (Round r in rounds)
+            {
+                sumOfPoints += r.PlayerScore;
+            }
+            Console.WriteLine($"Total Score: {sumOfPoints}");
+        }
+
+        static ChosenFigure CalculateChosenFigure(char oponent, char result)
+        {
+            switch (oponent)
+            {
+                //A = Rock
+                case 'A':
+                    switch (result)
+                    {
+                        case 'X':
+                            return ChosenFigure.Scissors;
+                        case 'Y':
+                            return ChosenFigure.Rock;
+                        case 'Z':
+                            return ChosenFigure.Paper;
+                    }
+                    break;
+                //B = Paper
+                case 'B':
+                    switch (result)
+                    {
+                        case 'X':
+                            return ChosenFigure.Rock;
+                        case 'Y':
+                            return ChosenFigure.Paper;
+                        case 'Z':
+                            return ChosenFigure.Scissors;
+                    }
+                    break;
+                //C = Scissors
+                case 'C':
+                    switch (result)
+                    {
+                        case 'X':
+                            return ChosenFigure.Paper;
+                        case 'Y':
+                            return ChosenFigure.Scissors;
+                        case 'Z':
+                            return ChosenFigure.Rock;
+                    }
+                    break;
+            }
+            //placeholdeer bcs reasons
+            return ChosenFigure.Rock;
+        }
+    }
+}
---
+++ Day2/Round.cs
@@ -0,0 +1,112 @@
+﻿namespace Day2
+{
+    enum ChosenFigure
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+    internal class Round
+    {
+        ChosenFigure oponent;
+        ChosenFigure player;
+        int playerScore;
+        public Round(char oponent, char player)
+        {
+            switch(oponent)
+            {
+                case 'A':
+                    this.oponent = ChosenFigure.Rock;
+                    break;
+                case 'B':
+                    this.oponent = ChosenFigure.Paper;
+                    break;
+                case 'C':
+                    this.oponent = ChosenFigure.Scissors;
+                    break;
+            }
+            switch (player)
+            {
+                case 'X':
+                    this.player = ChosenFigure.Rock;
+                    break;
+                case 'Y':
+                    this.player = ChosenFigure.Paper;
+                    break;
+                case 'Z':
+                    this.player = ChosenFigure.Scissors;
+                    break;
+            }
+            playerScore = MatchResultPoints();
+        }
+
+        public Round(char oponent, ChosenFigure player)
+        {
+            switch (oponent)
+            {
+                case 'A':
+                    this.oponent = ChosenFigure.Rock;
+                    break;
+                case 'B':
+                    this.oponent = ChosenFigure.Paper;
+                    break;
+                case 'C':
+                    this.oponent = ChosenFigure.Scissors;
+                    break;
+            }
+            this.player = player;
+            playerScore = MatchResultPoints();
+        }
+
+        public int PlayerScore
+        {
+            get 
+            { 
+                return playerScore; 
+            }
+        }
+        int MatchResultPoints()
+        {
+            switch(player)
+            {
+                //add 1 to every return
+                case ChosenFigure.Rock:
+                    switch(oponent)
+                    {
+                        case ChosenFigure.Rock:
+                            return 4;
+                        case ChosenFigure.Paper:
+                            return 1;
+                        case ChosenFigure.Scissors:
+                            return 7;
+                    }
+                    break;
+                //add 2 in every return
+                case ChosenFigure.Paper:
+                    switch (oponent)
+                    {
+                        case ChosenFigure.Rock:
+                            return 8;
+                        case ChosenFigure.Paper:
+                            return 5;
+                        case ChosenFigure.Scissors:
+                            return 2;
+                    }
+                    break;
+                //add 3 in every return
+                case ChosenFigure.Scissors:
+                    switch (oponent)
+                    {
+                        case ChosenFigure.Rock:
+                            return 3;
+                        case ChosenFigure.Paper:
+                            return 9;
+                        case ChosenFigure.Scissors:
+                            return 6;
+                    }
+                    break;
+            }
+            return 0;
+        }
+    }
+}
