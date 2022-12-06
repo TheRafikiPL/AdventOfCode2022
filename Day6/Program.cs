8a15414ef4a9b48f5505d36943af458fb7304d78
TheRafikiPL <48444061+TheRafikiPL@users.noreply.github.com>
2022-12-06 09:56:36 +0100

    Day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Day6
+{
+    internal class Program
+    {
+        static string file = "signal.txt";
+        static void CalculateSignal(int numberOfLetters)
+        {
+            StreamReader sr = new StreamReader($@"Data\{file}");
+            string line = sr.ReadLine();
+
+            for (int i = 0; i < line.Length - numberOfLetters; i++)
+            {
+                bool isgood = true;
+                List<char> letters = new List<char>();
+                for (int j = 0; j < numberOfLetters; j++)
+                {
+                    if (letters.Contains(line[i + j]))
+                    {
+                        isgood = false;
+                        break;
+                    }
+                    letters.Add(line[i + j]);
+                }
+                if (isgood)
+                {
+                    Console.WriteLine(i + numberOfLetters);
+                    break;
+                }
+            }
+            sr.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            //PART 1
+            Console.WriteLine("PART 1");
+            CalculateSignal(4);
+            
+            //PART 2
+            Console.WriteLine("PART 2");
+            CalculateSignal(14);
+        }
+    }
+}
